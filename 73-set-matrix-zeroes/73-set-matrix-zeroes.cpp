080ed9219627ee7b33c77e68ee547a064392333f
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-02-14 13:52:16 +0530

    Time: 12 ms (90.61%), Space: 13.3 MB (54.39%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& mat) {
+        bool f=false;
+        int n=mat.size();
+        int m=mat[0].size();
+        for(int i=0;i<n;i++){
+            if(mat[i][0]==0){
+                f=true;
+            }
+            for(int j=1;j<m;j++){
+                if(mat[i][j]==0){
+                    mat[i][0]=0;
+                    mat[0][j]=0;
+                }
+            }
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>0;j--){
+                if(mat[i][0]==0 || mat[0][j]==0){
+                    mat[i][j]=0;
+                }
+            }
+            if(f==true){
+                mat[i][0]=0;
+            }
+        }
+    }
+};
