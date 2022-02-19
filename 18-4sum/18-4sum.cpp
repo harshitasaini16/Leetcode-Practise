db60c7f4059cfc95a88a3972a49497ce9ce688c6
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-02-19 15:05:44 +0530

    Time: 64 ms (75.34%), Space: 12.9 MB (98.48%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -3,17 +3,29 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        set<vector<int>>s;
+        vector<vector<int>>v;
         for(int i=0;i<n-3;i++){
+            if(i!=0 && nums[i]==nums[i-1]){
+                continue;
+            }
             for(int j=i+1;j<n-2;j++){
+                if(j!=i+1 && nums[j]==nums[j-1]){
+                    continue;
+                }
                 int k=j+1;
                 int l=n-1;
                 while(k<l){
                     int x=(long long)nums[i]+nums[j]+nums[k]+nums[l];
                     if(x==target){
-                        s.insert({nums[i],nums[j],nums[k],nums[l]});
-                        k++;
-                        l--;
+                        v.push_back({nums[i],nums[j],nums[k],nums[l]});
+                        int a=nums[k];
+                        int b=nums[l];
+                        while(k<l && a==nums[k]){
+                            k++;
+                        }
+                        while(k<l && b==nums[l]){
+                            l--;
+                        }
                     }
                     else if(x>target){
                         l--;
@@ -23,10 +35,6 @@ public:
                     }
                 }
             }
-        }
-        vector<vector<int>>v;
-        for(auto i:s){
-            v.push_back(i);
         }
         return v;
     }
