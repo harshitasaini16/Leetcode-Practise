786426e6326d4995646005bcb634fcbbec906843
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-02-26 10:25:22 +0530

    Time: 528 ms (10.75%), Space: 139.1 MB (6.16%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        int x=1;
+        while(m[x]!=0){
+            x++;
+        }
+        return x;
+    }
+};
