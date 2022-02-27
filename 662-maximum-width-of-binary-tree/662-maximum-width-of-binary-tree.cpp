247dd19ef6697c514d4ee9e8edcd2f8aaff2a14b
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-02-27 10:17:01 +0530

    Time: 10 ms (69.33%), Space: 17.3 MB (79.50%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,46 +1,35 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
         if(root==NULL){
             return 0;
         }
-        queue<pair<TreeNode*,int>>q;
+        queue<pair<TreeNode*,long long  >>q;
         q.push({root,0});
-        int ans=0;
+        long long ans=1;
         while(!q.empty()){
-            int size=q.size();
-            int mn,mx;
-            int x=q.front().second;
-            for(int i=0;i<size;i++){
-                int id=q.front().second-x;
+            long long k=q.size();
+            long long  minn=q.front().second;
+            long long f,l;
+            for(int i=0;i<k;i++){
+                long long id=q.front().second-minn;
                 TreeNode*tt=q.front().first;
                 q.pop();
                 if(i==0){
-                    mn=id;
+                    f=id;
                 }
-                if(i==size-1){
-                    mx=id;
+                if(i==k-1){
+                    l=id;
                 }
                 if(tt->left){
-                    q.push({tt->left,(long long)2*id +1});
+                    q.push({tt->left,2*id +1});
                 }
                 if(tt->right){
-                    q.push({tt->right,(long long)2*id +2});
+                    q.push({tt->right,2*id +2});
                 }
             }
-            ans=max(ans,1+(mx-mn));
+            ans=max(ans,(l-f)+1);
         }
-        return ans;
+        return (int)ans;
     }
 };
