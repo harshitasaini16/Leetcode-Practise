081c72c93be5ad1863aebb7b7da515b2f81831dd
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-03-01 11:42:27 +0530

    Time: 4 ms (88.16%), Space: 7.9 MB (48.88%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int countSetBits(int n)
-{
-    // base case
-    if (n == 0)
-        return 0;
-    else
-        return 1 + countSetBits(n & (n - 1));
-}
     vector<int> countBits(int n) {
-        vector<int>v;
-        for(int i=0;i<=n;i++){
-            int x=countSetBits(i);
-            v.push_back(x);
+        vector<int>v(n+1,0);
+        for(int i=1;i<=n;i++){
+            v[i]=v[i&(i-1)]+1;
         }
         return v;
     }
 };
+// class Solution {
+// public:
+//     vector<int> countBits(int n) {
+//         vector<int> ans(n+1, 0);
+//         for (int i = 1; i <= n; ++i)
+//             ans[i] = ans[i&(i-1)] + 1;
+//         return ans;
+//     }
+// };
