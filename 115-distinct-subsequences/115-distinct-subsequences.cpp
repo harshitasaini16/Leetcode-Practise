d3103f10a1ae4f25c548f9d97e2833ea927f1d8e
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-03-07 09:47:23 +0530

    Time: 12 ms (95.62%), Space: 10.7 MB (87.48%) - LeetHub

---
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n=s.length();
+        int m=t.length();
+        int mat[n+1][m+1];
+        int mod=1e9+7;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 && j==0){
+                    mat[i][j]=1;
+                }
+                else if(i==0){
+                    mat[i][j]=0;
+                }
+                else if(j==0){
+                    mat[i][j]=1;
+                }
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    mat[i][j]=(mat[i-1][j-1]+mat[i-1][j])%mod;
+                }
+                else{
+                    mat[i][j]=(mat[i-1][j])%mod;
+                }
+            }
+        }
+        return mat[n][m];
+    }
+};
