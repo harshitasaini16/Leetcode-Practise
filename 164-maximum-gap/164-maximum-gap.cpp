f0fc1cac8f8e895f6c2d3c5da7682e91fd19bcb0
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-03-09 10:51:59 +0530

    Time: 189 ms (81.50%), Space: 88 MB (19.76%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.cpp
+++ 164-maximum-gap/164-maximum-gap.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        priority_queue<int>q;
-        for(int i=0;i<nums.size();i++){
-            q.push(nums[i]);
+        int n=nums.size();
+        if(n<2){
+            return 0;
         }
-        int mx=0;
-        int x=q.top();
-        q.pop();
-        while(!q.empty()){
-            int y=q.top();
-            q.pop();
-            mx=max(mx,abs(x-y));
-            x=y;
+        int mn=nums[0];
+        int mx=nums[0];
+        for(int i=0;i<n;i++){
+            mx=max(mx,nums[i]);
+            mn=min(mn,nums[i]);
         }
-        return mx;
+        int interval=(mx-mn)/(n-1);
+        if((mx-mn)%(n-1)!=0){
+            interval++;
+        }
+        if(interval==0){
+            return 0;
+        }
+        vector<int>minn(n,INT_MAX);
+        vector<int>maxx(n,INT_MIN);
+        for(int i=0;i<n;i++){
+            int x=(nums[i]-mn)/interval;
+            minn[x]=min(minn[x],nums[i]);
+            maxx[x]=max(maxx[x],nums[i]);
+        }
+        vector<int>minn2,maxx2;
+        for(int i=0;i<n;i++){
+            if(minn[i]!=INT_MAX && maxx[i]!=INT_MIN){
+                minn2.push_back(minn[i]);
+                maxx2.push_back(maxx[i]);
+            }
+        }
+        int ans=0;
+        for(int i=1;i<minn2.size();i++){
+            ans=max(ans,minn2[i]-maxx2[i-1]);
+        }
+        return ans;
     }
 };
