a50fe3eea55aff7feae26ad07a0fe6102e7ac13c
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-03-11 09:43:12 +0530

    Time: 86 ms (73.00%), Space: 31.7 MB (25.00%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,40 +1,35 @@
-  class Solution {
+class Solution {
 public:
-	vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
-    int r=grid.size();
-    int c=grid[0].size();
-    
-    queue<pair<int,int>> q;
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            if(grid[i][j]==0){
-                q.push({i,j});
-            }
-            else grid[i][j]=-1;
-        }
-    }
-    int front=0;
-    int dx[4]={-1,1,0,0};
-    int dy[4]={0,0,-1,1};
-    //if(q.empty() and front) return -1;
-    while(!q.empty()){
-        int n=q.size();
-        front++;
+    int dx[4]={-1,0,0,1};
+    int dy[4]={0,1,-1,0};
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        queue<pair<pair<int,int>,int>>q;
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>ans(n,vector<int>(m));
         for(int i=0;i<n;i++){
-        pair<int,int> p=q.front();
-        q.pop();
-        for(int i=0;i<4;i++){
-            int x=dx[i]+p.first;
-            int y=dy[i]+p.second;
-            if(x>-1 and y>-1 and x<r and y<c and grid[x][y]==-1){
-                grid[x][y]=front;
-                q.push({x,y});
-                //front--;
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    q.push({{i,j},0});
+                    ans[i][j]=0;
+                }
             }
         }
+        while(!q.empty()){
+            int x=q.front().first.first;
+            int y=q.front().first.second;
+            int s=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int xx=x+dx[i];
+                int yy=y+dy[i];
+                if(xx<n && yy<m && xx>=0 && yy>=0 && mat[xx][yy]==1){
+                    mat[xx][yy]=0;
+                    ans[xx][yy]=s+1;
+                    q.push({{xx,yy},s+1});
+                }
+            }
         }
-        
+        return ans;
     }
-   return grid; 
-}
 };
