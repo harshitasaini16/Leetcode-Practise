f598e6735841e6d53db2953f5c47ba1e43ee8317
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-03-12 15:15:15 +0530

    Time: 8 ms (84.20%), Space: 11.2 MB (60.48%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,31 +17,31 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(!head) return NULL;
-        
-        Node* temp = head;
-        
-        while(temp){
-            Node* node = new Node(temp->val);
-            node->next = temp->next;
-            temp->next = node;
-            temp = node->next;
+        if(head==NULL){
+            return NULL;
         }
-        
-        temp = head;
-        while(temp){
-            temp->next->random = (temp->random)?temp->random->next:NULL;
-            temp = temp->next->next;
+        Node *temp=head;
+        while(temp!=NULL){
+            Node *node=new Node(temp->val);
+            node->next=temp->next;
+            temp->next=node;
+            temp=node->next;
         }
-        
-        Node* ans = head->next;
-        temp = head->next;
+        temp=head;
+        while(temp!=NULL){
+            temp->next->random=(temp->random)?temp->random->next:NULL;
+            temp=temp->next->next;
+        }
+        Node *ans=head->next;
+        temp=head->next;
         while(head){
-            head->next = temp->next;
-            head = head->next;
-            if(!head) break;
-            temp->next = head->next;
-            temp = temp->next;
+            head->next=temp->next;
+            head=head->next;
+            if(head==NULL){
+                break;
+            }
+            temp->next=head->next;
+            temp=temp->next;
         }
         return ans;
     }
