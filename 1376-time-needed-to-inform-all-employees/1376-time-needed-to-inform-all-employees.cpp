c3556b79d96b30ae5b51fd1597027d4818720829
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-03-15 09:44:15 +0530

    Time: 1842 ms (5.04%), Space: 165.9 MB (5.57%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int numOfMinutes(int n, int headID, vector<int>& man, vector<int>& inform) {
+    int numOfMinutes(int n, int headID, vector<int>& man, vector<int>& informTime) {
         map<int,vector<int>>parent;
-        int p=0;
         for(int i=0;i<man.size();i++){
             parent[man[i]].push_back(i);
-            if(man[i]==-1){
-                p=i;
-            }
         }
+        // map<int,int>vis;
+        // vis[headID]++;
         queue<pair<int,int>>q;
-        q.push({p,0});
-        int ans=INT_MIN;
+        q.push({headID,0});
+        int mx=INT_MIN;
         while(!q.empty()){
             int x=q.front().first;
             int y=q.front().second;
-            ans=max(ans,y);
+            mx=max(mx,y);
             q.pop();
-            for(int i=0;i<parent[x].size();i++){
-                q.push({parent[x][i],y+inform[x]});
+            for(int j=0;j<parent[x].size();j++){
+                q.push({parent[x][j],y+informTime[x]});
             }
         }
-        return ans;
+        return mx;
     }
 };
