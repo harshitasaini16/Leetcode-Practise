c47e90989f910cb4f4af0b11f26884ef68f2339b
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-03-17 09:43:58 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (8.12%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
     int scoreOfParentheses(string s) {
-        stack<int>stk;
-        for(int i=0;i<s.length();i++){
+        int n=s.length();
+        stack<int>st;
+        for(int i=0;i<n;i++){
             if(s[i]=='('){
-                stk.push(-1);
-            }else{
-                if(stk.top()==-1){
-                    stk.pop();
-                    stk.push(1);
-                }else {
-                    int t=stk.top();
-                    stk.pop();
-                    if(stk.empty()==false and stk.top()==-1){
-                        stk.pop();
-                        stk.push(2*t);
-                    }else{
-                        while(stk.empty()==false and stk.top()!=-1){
-                            t+=stk.top();
-                            stk.pop();
+                st.push(-1);
+            }
+            else{
+                if(st.top()==-1){
+                    st.pop();
+                    st.push(1);
+                }
+                else{
+                    int t=st.top();
+                    st.pop();
+                    if(!st.empty() && st.top()==-1){
+                        st.pop();
+                        st.push(2*t);
+                    }
+                    else{
+                        while(!st.empty() && st.top()!=-1){
+                            t+=st.top();
+                            st.pop();
                         }
-                        if(stk.empty()==false and stk.top()==-1){
-                            stk.pop();
-                            stk.push(2*t);
-                        }else{
-                            stk.push(2*t);
+                        if(!st.empty() && st.top()==-1){
+                            st.pop();
+                            st.push(2*t);
+                        }
+                        else{
+                            st.push(2*t);
                         }
                     }
                 }
             }
         }
-        int ans=0;
-        while(stk.empty()==false){
-            ans+=stk.top();
-            stk.pop();
+        int sum=0;
+        while(!st.empty()){
+            sum+=st.top();
+            st.pop();
         }
-        return ans;
+        return sum;
     }
 };
