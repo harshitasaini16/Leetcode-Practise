cae128110f6f58df0857b25b2e1ac9295dd218a5
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-03-26 10:13:40 +0530

    Time: 1287 ms (5.08%), Space: 11.2 MB (73.82%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/386-lexicographical-numbers.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
+    static bool cmp(int n,int m){
+        string s1=to_string(n);
+        string s2=to_string(m);
+        int i=0;
+        int j=0;
+        while(i<n && j<m){
+            if(s1[i]<s2[j]){
+                return true;
+            }
+            else if(s1[i]>s2[j]){
+                return false;
+            }
+            i++;
+            j++;
+        }
+        if(n<m){
+            return true;
+        }
+        return false;
+    }
     vector<int> lexicalOrder(int n) {
-        vector<string>v;
-        for(int i=1;i<=n;i++){
-            v.push_back(to_string(i));
-        }
-        sort(v.begin(),v.end());
         vector<int>ans;
-        for(int i=0;i<v.size();i++){
-            ans.push_back(stoi(v[i]));
+        for(int i=1;i<=n;i++){
+            ans.push_back(i);
         }
+        sort(ans.begin(),ans.end(),cmp);
         return ans;
     }
 };
