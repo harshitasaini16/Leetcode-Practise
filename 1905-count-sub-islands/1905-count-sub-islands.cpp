06705ac67ccefdb0d8a05f1c410aa51d0a09164d
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-03-28 15:08:24 +0530

    Time: 806 ms (6.11%), Space: 133.6 MB (15.72%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
-    void fun(int x,int y,vector<vector<int>>& grid1, vector<vector<int>>& grid2,int n,int m,int &g){
-        if(x<0 || y<0 || x>=n || y>=m){
-            return;
+    map<pair<int,int>,int>mp;
+    bool fun(int x,int y,vector<vector<int>>& grid1, vector<vector<int>>& grid2,int n,int m){
+        if(x<0 || y<0 || x>=n || y>=m || grid2[x][y]==0){
+            return true;
         }
-        if(grid2[x][y]==0){
-            return;
+        if(mp.find({x,y})!=mp.end()){
+            return mp[{x,y}];
         }
         if(grid2[x][y]==1 && grid1[x][y]==0){
-            g++;
-            return;
+            return false;
         }
+        int p=grid1[x][y];
+        int q=grid2[x][y];
+        grid1[x][y]=0;
         grid2[x][y]=0;
-        fun(x+1,y,grid1,grid2,n,m,g);
-        fun(x-1,y,grid1,grid2,n,m,g);
-        fun(x,y+1,grid1,grid2,n,m,g);
-        fun(x,y-1,grid1,grid2,n,m,g);
+        bool f=(fun(x+1,y,grid1,grid2,n,m) && fun(x-1,y,grid1,grid2,n,m) && fun(x,y-1,grid1,grid2,n,m) && fun(x,y+1,grid1,grid2,n,m));
+        if(f==true){
+            return mp[{x,y}]=true;
+        }
+        grid1[x][y]=p;
+        grid2[x][y]=q;
+        return mp[{x,y}]=false;
     }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        int n=grid2.size();
-        int m=grid2[0].size();
+        mp.clear();
+        int n=grid1.size();
+        int m=grid1[0].size();
         int s=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid2[i][j]==1){
-                    int g=0;
-                    fun(i,j,grid1,grid2,n,m,g);
-                    if(g==0){
+                    if(fun(i,j,grid1,grid2,n,m)){
                         s++;
                     }
                 }
