9a6b6bfded857406ccc51d95e92bfc74de16f4b7
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-03-28 09:34:20 +0530

    Time: 9 ms (6.11%), Space: 5.8 MB (74.82%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-        if(num1==0 || num2==0){
-            return 0;
-        }
-        priority_queue<int>q;
-        q.push(num1);
-        q.push(num2);
-        int s=1;
-        while(true){
-            int x=q.top();
-            q.pop();
-            int y=q.top();
-            q.pop();
-            if(x==y){
-                return s;
+        int s=0;
+        while(num1!=0 && num2!=0){
+            if(num1>num2){
+                num1-=num2;
             }
-            q.push(y);
-            q.push(x-y);
+            else{
+                num2-=num1;
+            }
             s++;
         }
+        return s;
     }
 };
