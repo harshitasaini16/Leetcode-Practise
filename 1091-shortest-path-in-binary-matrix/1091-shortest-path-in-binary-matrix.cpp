f058127acfbcd5a4049141c7a3336848463e7207
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-03-29 14:59:52 +0530

    Time: 101 ms (54.05%), Space: 20.2 MB (58.28%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -4,27 +4,26 @@ public:
     int dy[8]={-1,0,1,1,1,0,-1,-1};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
-        int m=grid[0].size();
         queue<pair<pair<int,int>,int>>q;
-        if(grid[0][0]==1 || grid[n-1][m-1]==1){
+        if(grid[0][0]!=0 || grid[n-1][n-1]!=0){
             return -1;
         }
-        map<pair<int,int>,int>vis;
         q.push({{0,0},1});
-        vis[{0,0}]++;
+        // map<pair<int,int>,int>vis;
+        // vis[{0,0}]++;
         while(!q.empty()){
             int x=q.front().first.first;
             int y=q.front().first.second;
             int s=q.front().second;
-            if(x==n-1 && y==m-1){
+            q.pop();
+            if(x==n-1 && y==n-1){
                 return s;
             }
-            q.pop();
             for(int i=0;i<8;i++){
                 int xx=x+dx[i];
                 int yy=y+dy[i];
-                if(xx>=0 && yy>=0 && xx<n && yy<m && grid[x][y]==0 && vis[{xx,yy}]==0){
-                    vis[{xx,yy}]++;
+                if(xx>=0 && yy>=0 && xx<n && yy<n && grid[xx][yy]==0){
+                    grid[xx][yy]=1;
                     q.push({{xx,yy},s+1});
                 }
             }
