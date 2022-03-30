ab5222339937a26b9b2fc18459c3ba2e17220c81
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-03-30 17:03:29 +0530

    Time: 28 ms (93.42%), Space: 89.3 MB (63.51%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    float fun(vector<int>& nums1, int n,vector<int>& nums2,int m){
+    double fun(vector<int>& nums1,int n,vector<int>& nums2,int m){
         if(m<n){
             return fun(nums2,m,nums1,n);
         }
-        double l=0;
-        double r=n;
+        int l=0;
+        int r=n;
         int mid=(n+m+1)/2;
         while(l<=r){
             int cut1=l+(r-l)/2;
@@ -15,11 +15,11 @@ public:
             int r1=(cut1==n)?INT_MAX:nums1[cut1];
             int r2=(cut2==m)?INT_MAX:nums2[cut2];
             if(l1<=r2 && l2<=r1){
-                if((n+m)%2!=0){
-                    return max(l1,l2);
+                if((n+m)%2==0){
+                    return (double)(max(l1,l2)+min(r1,r2))/2;
                 }
                 else{
-                    return (max(l1,l2)+min(r1,r2))/2.0;
+                    return (double)(max(l1,l2));
                 }
             }
             else if(l1>r2){
@@ -32,8 +32,6 @@ public:
         return 0.0;
     }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
-        return fun(nums1,n,nums2,m);
+        return fun(nums1,nums1.size(),nums2,nums2.size());
     }
 };
