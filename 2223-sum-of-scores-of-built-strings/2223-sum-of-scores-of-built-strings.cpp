a3ac67f7f2a983aebfc4b574fb37cfbf67177bb2
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-04-07 10:17:52 +0530

    Time: 72 ms (71.27%), Space: 20.7 MB (60.69%) - LeetHub

--- 2223-sum-of-scores-of-built-strings/2223-sum-of-scores-of-built-strings.cpp
+++ 2223-sum-of-scores-of-built-strings/2223-sum-of-scores-of-built-strings.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     long long sumScores(string s) {
         int n=s.length();
-    vector<int>z(n);
-    for(int i=1,l=0,r=0; i<n; i++){
-        if(i<=r){
-            z[i]=min(r-i+1,z[i-l]);
+        vector<int>z(n);
+        for(int i=1,l=0,r=0; i<n ;i++){
+            if(i<=r){
+                z[i]=min(r-i+1,z[i-l]);
+            }
+            while(i+z[i]<n && s[z[i]]==s[z[i]+i]){
+                z[i]++;
+            }
+            if(i+z[i]-1>r){
+                l=i;
+                r=z[i]+i-1;
+            }
         }
-        while(i+z[i]<n && s[z[i]]==s[i+z[i]]){
-            z[i]++;
+        long long ans=0;
+        for(int i=0;i<n;i++){
+            ans+=z[i];
         }
-        if(i+z[i]-1>r){
-            l=i;
-            r=i+z[i]-1;
-        }
-    }
-    long long ans=0;
-    for(int i=0;i<n;i++){
-        ans+=z[i];
-    }
         return ans+s.length();
     }
 };
