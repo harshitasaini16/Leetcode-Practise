41abe2d02ac300d76af75b5ee584bd1e3bfd40f7
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-04-08 09:50:25 +0530

    Time: 60 ms (81.13%), Space: 12.8 MB (64.78%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,50 +1,33 @@
 class Solution {
 public:
-    int dp[72][72][72];
     int dy[3]={-1,0,1};
-    int fun(int x,int y1,int y2,vector<vector<int>>& grid,int n,int m){
-        if(x==n-1){
-            if(y1==y2){
-                return grid[x][y1];
-            }
-            return grid[x][y1]+grid[x][y2];
+    int dp[100][100][100];
+    int fun(int x,int y1,int y2,int n,int m,vector<vector<int>>& grid){
+        if(x==n){
+            return 0;
         }
         if(dp[x][y1][y2]!=-1){
             return dp[x][y1][y2];
         }
+        int s=0;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                int yy1=y1+dy[i];
+                int yy2=y2+dy[j];
+                if(yy1>=0 && yy2>=0 && yy1<m && yy2<m){
+                    s=max(s,fun(x+1,yy1,yy2,n,m,grid));
+                }
+            }
+        }
         if(y1==y2){
-            int s=grid[x][y1];
-            int ans=0;
-            for(int i=0;i<3;i++){
-                for(int j=0;j<3;j++){
-                    int yy1=y1+dy[i];
-                    int yy2=y2+dy[j];
-                    if(yy1>=0 && yy2>=0 && yy1<m && yy2<m){
-                        ans=max(ans,fun(x+1,yy1,yy2,grid,n,m));
-                    }
-                }
-            }
-            return dp[x][y1][y2]=s+ans;
+            return dp[x][y1][y2]=grid[x][y1]+s;
         }
-        else{
-            int s=grid[x][y1]+grid[x][y2];
-            int ans=0;
-            for(int i=0;i<3;i++){
-                for(int j=0;j<3;j++){
-                    int yy1=y1+dy[i];
-                    int yy2=y2+dy[j];
-                    if(yy1>=0 && yy2>=0 && yy1<m && yy2<m){
-                        ans=max(ans,fun(x+1,yy1,yy2,grid,n,m));
-                    }
-                }
-            }
-            return dp[x][y1][y2]=s+ans;
-        }
+        return dp[x][y1][y2]=grid[x][y1]+grid[x][y2]+s;
     }
     int cherryPickup(vector<vector<int>>& grid) {
         memset(dp,-1,sizeof(dp));
         int n=grid.size();
         int m=grid[0].size();
-        return fun(0,0,m-1,grid,n,m);
+        return fun(0,0,m-1,n,m,grid);
     }
 };
