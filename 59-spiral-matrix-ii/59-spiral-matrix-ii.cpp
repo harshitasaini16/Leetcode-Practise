8d77337ed98b1074db9c3203de0af1e579d3b468
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-04-13 09:26:30 +0530

    Time: 4 ms (19.27%), Space: 6.6 MB (63.66%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,44 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>>mat(n,vector<int>(n,0));
-        int l=0,r=n-1,u=0,d=n-1;
-        int x=1;
+        vector<vector<int>>ans(n,vector<int>(n,0));
+        int l=0,u=0,d=n-1,r=n-1;
         int y=0;
+        int x=1;
         while(l<=r && u<=d){
             if(y==0){
                 for(int i=l;i<=r;i++){
-                   mat[u][i]=x;
-                   x++;
+                    ans[u][i]=x;
+                    x++;
                 }
                 u++;
                 y++;
             }
             else if(y==1){
                 for(int i=u;i<=d;i++){
-                   mat[i][r]=x;
-                   x++;
+                    ans[i][r]=x;
+                    x++;
                 }
                 r--;
                 y++;
             }
             else if(y==2){
                 for(int i=r;i>=l;i--){
-                   mat[d][i]=x;
-                   x++;
+                    ans[d][i]=x;
+                    x++;
                 }
                 d--;
                 y++;
             }
             else if(y==3){
                 for(int i=d;i>=u;i--){
-                   mat[i][l]=x;
-                   x++;
+                    ans[i][l]=x;
+                    x++;
                 }
                 l++;
                 y=0;
             }
         }
-        return mat;
+        return ans;
     }
 };
