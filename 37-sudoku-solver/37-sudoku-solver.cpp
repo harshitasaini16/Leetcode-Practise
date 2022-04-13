02a20b60fe08cbfaa586daef6856b419f1241260
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-04-13 09:50:16 +0530

    Time: 29 ms (65.62%), Space: 6.4 MB (53.67%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,42 +1,43 @@
 class Solution {
 public:
-    bool check(vector<vector<char>>&arr,int i,int j,char c){
-        int r=i-i%3,cc=j-j%3;
+    bool valid(vector<vector<char>>&arr,int i,int j,int val){
         for(int k=0;k<9;k++){
-            if(arr[k][j]==c or arr[i][k]==c){
+            if(arr[i][k]==val or arr[k][j]==val){
                 return false;
-            }
+            } 
         }
+        int x=i-i%3,y=j-j%3;
         for(int k=0;k<3;k++){
             for(int kk=0;kk<3;kk++){
-                if(arr[k+r][kk+cc]==c){
+                if(arr[k+x][kk+y]==val){
                     return false;
                 }
             }
         }
         return true;
     }
-    bool fun(vector<vector<char>>&arr,int i,int j){
-        if(i==9){
+    bool fun(int x,int y,vector<vector<char>>& board){
+        if(x==9){
             return true;
         }
-        if(j==9){
-            return fun(arr,i+1,0);
+        if(y==9){
+            return fun(x+1,0,board);
         }
-        if(arr[i][j]!='.'){
-            return fun(arr,i,j+1);
+        if(board[x][y]!='.'){
+            return fun(x,y+1,board);
         }
-        for(char c='1' ;c<='9';c++ ){
-            if(check(arr,i,j,c)==true){
-                arr[i][j]=c;
-                if(fun(arr,i,j+1))
+        for(int c='1';c<='9';c++){
+            if(valid(board,x,y,c)){
+                board[x][y]=c;
+                if(fun(x,y+1,board)){
                     return true;
-                arr[i][j]='.';
+                }
+                board[x][y]='.';
             }
         }
         return false;
     }
     void solveSudoku(vector<vector<char>>& board) {
-        fun(board,0,0);
+        fun(0,0,board);
     }
 };
