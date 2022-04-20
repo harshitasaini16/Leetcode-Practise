471928b41b9ea78f3a3d48a9d8c13668a7405fd2
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-04-20 09:21:53 +0530

    Time: 384 ms (35.14%), Space: 24.1 MB (36.97%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     int dp[2001][2001];
-    bool check(string &s,int x,int y){
-       if(dp[x][y]!=-1){
-           return 0;
-       }
-        while(x<y){
-            if(s[x]!=s[y]){
+    bool check(int i,int j,string &s){
+        if(dp[i][j]!=-1){
+            return 0;
+        }
+        while(i<j){
+            if(s[i]!=s[j]){
                 return false;
             }
-            x++;
-            y--;
+            i++;
+            j--;
         }
         return true;
     }
-    int fun(int x,int n,string &s){
-        if(x>=n || check(s,x,n)){
-            return dp[x][n]=0;
+    int fun(int x,int y,string &s){
+        if(x>=y){
+            return 0;
         }
-        if(dp[x][n]!=-1){
-            return dp[x][n];
+        if(check(x,y,s)){
+            return 0;
+        }
+        if(dp[x][y]!=-1){
+            return dp[x][y];
         }
         int ans=INT_MAX;
-        for(int i=x;i<n;i++){
-            if(check(s,x,i)){
-                ans=min(ans,1+fun(i+1,n,s));
+        for(int i=x;i<=y;i++){
+            if(check(x,i,s)){
+                ans=min(ans,fun(i+1,y,s));
             }
         }
-        return dp[x][n]=ans;
+        return dp[x][y]=ans+1;
     }
     int minCut(string s) {
         memset(dp,-1,sizeof(dp));
