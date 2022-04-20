d77843964c124e3004ca385edaf57abbf2a98212
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-04-20 10:34:42 +0530

    Time: 8 ms (26.82%), Space: 10.2 MB (72.18%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,45 +1,17 @@
 class Solution {
 public:
-    int fun(int l,int r,vector<int>nums,int n){
-        if(l<=r){
-            if(nums[l]<nums[r]){
-                return nums[l];
-            }
+    int findMin(vector<int>& nums) {
+       int l=0;
+        int r=nums.size()-1;
+        while(l+1<r){
             int mid=l+(r-l)/2;
-            if(mid==0){
-                if(nums[mid]<nums[n-1]){
-                    return nums[mid];
-                }
-                else{
-                    return fun(mid+1,r,nums,n);
-                }
-            }
-            else if(mid==n-1){
-                if(nums[mid]<nums[0] && nums[mid]<nums[mid-1]){
-                    return nums[mid];
-                }
-                else{
-                    return fun(l,mid-1,nums,n);
-                }
+            if(nums[mid]>nums[r]){
+                l=mid;
             }
             else{
-                if(nums[mid]<nums[mid+1] && nums[mid]<nums[mid-1]){
-                    return nums[mid];
-                }
-                if(nums[0]<nums[mid]){
-                    return fun(mid+1,r,nums,n);
-                }
-                else{
-                    return fun(l,mid-1,nums,n);
-                }
+                r=mid;
             }
         }
-        return -1;
-    }
-    int findMin(vector<int>& nums) {
-        if(nums.size()==1){
-            return nums[0];
-        }
-        return fun(0,nums.size()-1,nums,nums.size());
+        return min(nums[l],nums[r]);
     }
 };
