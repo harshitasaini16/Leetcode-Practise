bbd26d4504b620c2e995970ea9113f9b17ac055f
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-04-23 15:40:47 +0530

    Time: 722 ms (26.50%), Space: 116.1 MB (6.55%) - LeetHub

--- 282-expression-add-operators/282-expression-add-operators.cpp
+++ 282-expression-add-operators/282-expression-add-operators.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     vector<string>ans;
-    void fun(int x,string &s,string path,long res,long prev,int target){
+    void fun(int x,string &s,string path,long prev,long res,int target){
         if(x==s.length()){
             if(res==target){
                 ans.push_back(path);
             }
             return;
         }
-        string ns="";
         long temp=0;
-        for(int j=x;j<s.length();j++){
-            if(j>x && s[x]=='0'){
+        string g="";
+        for(int i=x;i<s.length();i++){
+            if(i>x && s[x]=='0'){
                 break;
             }
-            ns+=s[j];
-            temp=temp*10+(s[j]-'0');
+            temp=temp*10+(s[i]-'0');
+            g=g+s[i];
             if(x==0){
-                fun(j+1,s,path+ns,temp,temp,target);
+                fun(i+1,s,path+g,temp,temp,target);
             }
             else{
-                fun(j+1,s,path+"+"+ns, res+temp, temp,target);
-                fun(j+1,s,path+"-"+ns, res-temp, -temp,target);
-                fun(j+1,s,path+"*"+ns, res-prev+prev*temp, prev*temp,target);
+             fun(i+1,s,path+'+'+g,temp,res+temp,target);
+            fun(i+1,s,path+'-'+g,-temp,res-temp,target);
+            fun(i+1,s,path+'*'+g,temp*prev,res-prev+prev*temp,target);   
             }
         }
     }
     vector<string> addOperators(string num, int target) {
+        ans.clear();
         fun(0,num,"",0,0,target);
         return ans;
     }
-};  
+};
