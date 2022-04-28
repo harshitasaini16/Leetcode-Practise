f402fa21b80fc219f572c10819548b6b657e64df
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-04-28 15:02:41 +0530

    Time: 6 ms (43.36%), Space: 6.6 MB (60.15%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int>ans;
         int n=s.length();
+        vector<int>ans;
         map<char,int>m;
         for(int i=0;i<n;i++){
             m[s[i]]=i;
         }
-        int mx=0;
-        int x=-1;
+        int j=-1;
+        int mx=m[s[0]];
         for(int i=0;i<n;i++){
             mx=max(mx,m[s[i]]);
             if(mx==i){
-                ans.push_back(i-x);
-                x=i;
+                ans.push_back(i-j);
+                mx=m[s[i]];
+                j=i;
             }
         }
         return ans;
