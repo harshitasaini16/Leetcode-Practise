84425fc6d649bdf9ccd2193e947a86dd115a87c6
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-05-11 08:24:06 +0530

    Time: 1372 ms (5.11%), Space: 6 MB (58.60%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    map<pair<int,char>,int>dp;
-    int fun(int x,int n,string s,char c){
+    int ans;
+    void fun(int x,int n,char c){
         if(x==n){
-            return 1;
+            ans++;
+            return;
         }
-        if(dp.find({x,c})!=dp.end()){
-            return dp[{x,c}];
-        }
-        int ans=0;
+        string s="aeiou";
         for(int i=0;i<s.length();i++){
-            if(s[i]<=c){
-                ans+=fun(x+1,n,s,s[i]);
+            if(c<=s[i]){
+                fun(x+1,n,s[i]);
             }
         }
-        return dp[{x,c}]=ans;
     }
     int countVowelStrings(int n) {
-        string s="aeiou";
-        int ans=0;
-        dp.clear();
-        for(int i=0;i<s.length();i++){
-            ans+=fun(1,n,s,s[i]);
-        }
+        ans=0;
+        fun(0,n,' ');
         return ans;
     }
 };
