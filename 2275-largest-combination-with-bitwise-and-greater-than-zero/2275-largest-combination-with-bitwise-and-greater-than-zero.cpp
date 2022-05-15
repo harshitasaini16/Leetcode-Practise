e9f17b5fd282e096fd7d4c4f2830661e457eb383
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-05-15 09:38:54 +0530

    Time: 235 ms (33.33%), Space: 57.5 MB (33.33%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -3,7 +3,7 @@ public:
     int largestCombination(vector<int>& can) {
         int n=can.size();
         int ans=0;
-        for(int i=30;i>=0;i--){
+        for(int i=31;i>=0;i--){
             int c=0;
             for(int j=0;j<n;j++){
                 if((can[j]>>i&1)==1){
