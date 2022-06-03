cd570d349fc720d9aef0309da5e458397a243fca
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-06-03 17:17:51 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (51.67%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -3,20 +3,17 @@ public:
     bool isValid(string s) {
         stack<char>st;
         for(int i=0;i<s.length();i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
                 st.push(s[i]);
             }
             else{
-                if(st.empty()){
-                    return false;
-                }
-                else if(s[i]==')' && st.top()=='('){
+                if(!st.empty() && s[i]==')' && st.top()=='('){
                     st.pop();
                 }
-                else if(s[i]==']' && st.top()=='['){
+                else if(!st.empty() && s[i]==']' && st.top()=='['){
                     st.pop();
                 }
-                else if(s[i]=='}' && st.top()=='{'){
+                else if(!st.empty() && s[i]=='}' && st.top()=='{'){
                     st.pop();
                 }
                 else{
