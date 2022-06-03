5a018765133e017603483625f1d584e17e126c37
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-06-03 11:11:50 +0530

    Time: 607 ms (49.17%), Space: 156.2 MB (5.15%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,31 +1,26 @@
 class NumMatrix {
 public:
-    int n,m;
-    vector<vector<int>>sum;
-    NumMatrix(vector<vector<int>>& mat) {
-        n=mat.size();
-        m=mat[0].size();
+    vector<vector<int>>mat;
+    NumMatrix(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
         for(int i=0;i<=n;i++){
             vector<int>v;
             for(int j=0;j<=m;j++){
                 v.push_back(0);
             }
-            sum.push_back(v);
+            mat.push_back(v);
         }
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                sum[i][j]=sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1]+mat[i-1][j-1];
-                cout<<sum[i][j]<<' ';
+                mat[i][j]=mat[i-1][j]+mat[i][j-1]+matrix[i-1][j-1]-mat[i-1][j-1];
             }
-            cout<<endl;
         }
     }
-    int sumRegion(int r1, int c1, int r2, int c2) {
-        r1++;
-        r2++;
-        c1++;
-        c2++;
-        return sum[r2][c2]-sum[r2][c1-1]-sum[r1-1][c2]+sum[r1-1][c1-1];
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int s=mat[row2+1][col2+1]-mat[row2+1][col1]-mat[row1][col2+1]+mat[row1][col1];
+        return s;
     }
 };
 
