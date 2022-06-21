572f222c007dfcd26caf91ba2149c3b984065de2
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-06-21 09:22:29 +0530

    Time: 360 ms (36.08%), Space: 26.6 MB (36.49%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    int dp[201][10001];
-    bool fun(int n,vector<int>& nums,int x){
-        if(x==0){
-            return true;
-        }
-        if(dp[n][x]!=-1){
-            return dp[n][x];
-        }
-        if(n<=0){
+    int dp[201][20001];
+    bool fun(int x,vector<int>& nums,int s){
+        if(s<0){
             return false;
         }
-        if(nums[n-1]<=x){
-            return dp[n][x]=(fun(n-1,nums,x) || fun(n-1,nums,x-nums[n-1]));
+        if(s==0){
+            return true;
         }
-        else{
-            return dp[n][x]=fun(n-1,nums,x);
+        if(x==nums.size()){
+            return false;
         }
+        if(dp[x][s]!=-1){
+            return dp[x][s];
+        }
+        return dp[x][s]=(fun(x+1,nums,s-nums[x]) || fun(x+1,nums,s));
     }
     bool canPartition(vector<int>& nums) {
-        int s=0;
         memset(dp,-1,sizeof(dp));
+        int s=0;
         for(int i=0;i<nums.size();i++){
             s+=nums[i];
         }
         if(s%2!=0){
             return false;
         }
-        return fun(nums.size()-1,nums,s/2);
+        return fun(0,nums,s/2);
     }
 };
