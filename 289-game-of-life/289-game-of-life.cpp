7dccafc4a146a39f02607913fe456a1fb718e2ea
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-06-28 08:20:25 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (56.41%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
-    int dx[8]={0,1,1,-1,1,-1,-1,0};
-    int dy[8]={1,0,1,-1,-1,1,0,-1};
-    int fun(vector<vector<int>>& board,int n,int m,int x,int y){
-        int live=0;
-        for(int i=0;i<8;i++){
-            int xx=x+dx[i];
-            int yy=y+dy[i];
-            if(xx<n && yy<m && xx>=0 && yy>=0){
-                if(board[xx][yy]==1){
-                    live++;
-                }
-            }
-        }
-        return live;
-    }
-    void gameOfLife(vector<vector<int>>& board) {
-        vector<vector<int>>ans=board;
-        int n=board.size();
-        int m=board[0].size();
+    int dx[8]={1,-1,-1,1,0,1,0,-1};
+    int dy[8]={1,1,-1,-1,1,0,-1,0};
+    void gameOfLife(vector<vector<int>>& b) {
+        int n=b.size();
+        int m=b[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                int live=fun(board,n,m,i,j);
-                if(board[i][j]==1 && live<2){
-                    ans[i][j]=0;
+                int av=0;
+                for(int k=0;k<8;k++){
+                    int xx=dx[k]+i;
+                    int yy=dy[k]+j;
+                    if(xx<n && yy<m && xx>=0 && yy>=0 && (b[xx][yy]==1 || b[xx][yy]==3)){
+                        av++;
+                    }
                 }
-                else if(board[i][j]==1 && (live==2 || live==3)){
-                    ans[i][j]=1;
+                if(b[i][j]==1){
+                    if(av<2){
+                        b[i][j]=3;
+                    }
+                    else if(av>3){
+                        b[i][j]=3;
+                    }
                 }
-                else if(board[i][j]==1 && live>3){
-                    ans[i][j]=0;
-                }
-                else if(board[i][j]==0 && live==3){
-                    ans[i][j]=1;
+                else if(b[i][j]==0){
+                    if(av==3){
+                        b[i][j]=2;
+                    }
                 }
             }
         }
-        board=ans;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(b[i][j]==2){
+                    b[i][j]=1;
+                }
+                else if(b[i][j]==3){
+                    b[i][j]=0;
+                }
+            }
+        }
     }
 };
