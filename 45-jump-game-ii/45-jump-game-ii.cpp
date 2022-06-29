25cdc9d4dd689961ac8bbb7de2679170bd6460a3
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-06-29 08:48:21 +0530

    Time: 2114 ms (5.01%), Space: 17.2 MB (36.21%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int mat[10001];
-    int fun(int x,vector<int>&nums,int n){
-        if(x==n-1){
-            return 0;
-        }
-        if(x>=n){
-            return INT_MAX;
-        }
-        if(mat[x]!=-1){
-            return mat[x];
-        }
-        int y=INT_MAX-1;
-        for(int i=1;i<=nums[x];i++){
-            y=min(y,fun(x+i,nums,n));
-        }
-        return mat[x]=1+y;
-    }
     int jump(vector<int>& nums) {
-        memset(mat,-1,sizeof(mat));
-        return fun(0,nums,nums.size());
+        int x=1;
+        int n=nums.size();
+        vector<int>ans(n,INT_MAX);
+        ans[0]=0;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(j+nums[j]>=i){
+                    ans[i]=min(ans[i],ans[j]+1);
+                }
+            }
+        }
+        return ans[n-1];
     }
 };
