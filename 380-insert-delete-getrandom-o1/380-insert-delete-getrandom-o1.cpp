aa0358dc5a62409a6521b158021f4708431d1c1e
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-06-30 09:13:50 +0530

    Time: 329 ms (55.35%), Space: 97.2 MB (9.52%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
@@ -1,36 +1,39 @@
 class RandomizedSet {
 public:
     map<int,int>m;
+    vector<int>v;
     RandomizedSet() {
         m.clear();
+        v.clear();
     }
     
     bool insert(int val) {
-        if(m.find(val)!=m.end()){
-            return false;
-        }
-        else{
-            m[val]++;
+        if(m.find(val)==m.end()){
+            m[val]=v.size();
+            v.push_back(val);
             return true;
         }
+        return false;
     }
     
     bool remove(int val) {
         if(m.find(val)!=m.end()){
+            int n=v.size();
+            int x=m[val];
+            int y=n-1;
+            int vv=v[n-1];
+            m[vv]=x;
+            swap(v[x],v[y]);
+            v.pop_back();
             m.erase(val);
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
     
     int getRandom() {
-       vector<int>v;
-        for(auto i:m){
-            v.push_back(i.first);
-        }
-        return v[rand()%v.size()];
+        int x=(rand())%v.size();
+        return v[x];
     }
 };
 
