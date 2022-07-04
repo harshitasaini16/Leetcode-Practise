9f9c786c391449dcf25926a3500cbbe2dbb25f6d
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-07-04 22:04:21 +0530

    Time: 20 ms (93.75%), Space: 17.7 MB (84.26%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,61 +1,22 @@
 class Solution {
 public:
-    int candy(vector<int>& rating) {
-        int n=rating.size();
-        vector<int>v(n,1);
-        if(n==1){
-            return 1;
-        }
-        for(int i=0;i<n;i++){
-            if(i==0){
-                if(rating[i]>rating[i+1]){
-                    v[i]=1+v[i+1];
-                }
-            }
-            else if(i==n-1){
-                if(rating[i]>rating[i-1]){
-                    v[i]=1+v[i-1];
-                }
-            }
-            else{
-                if(rating[i]>rating[i-1] && rating[i]>rating[i+1]){
-                    v[i]=1+max(v[i-1],v[i+1]);
-                }
-                else if(rating[i]>rating[i-1]){
-                    v[i]=v[i-1]+1;
-                }
-                else if(rating[i]>rating[i+1]){
-                    v[i]=v[i+1]+1;
-                }
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int>ans(n,1);
+        for(int i=1;i<ratings.size();i++){
+            if(ratings[i-1]<ratings[i]){
+                ans[i]=ans[i-1]+1;
             }
         }
-        for(int i=n-1;i>=0;i--){
-            if(i==0){
-                if(rating[i]>rating[i+1]){
-                    v[i]=1+v[i+1];
-                }
-            }
-            else if(i==n-1){
-                if(rating[i]>rating[i-1]){
-                    v[i]=1+v[i-1];
-                }
-            }
-            else{
-                if(rating[i]>rating[i-1] && rating[i]>rating[i+1]){
-                    v[i]=1+max(v[i-1],v[i+1]);
-                }
-                else if(rating[i]>rating[i-1]){
-                    v[i]=v[i-1]+1;
-                }
-                else if(rating[i]>rating[i+1]){
-                    v[i]=v[i+1]+1;
-                }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                ans[i]=max(ans[i],ans[i+1]+1);
             }
         }
         int s=0;
-        for(int i=0;i<v.size();i++){
-            s+=v[i];
+        for(int i=0;i<ans.size();i++){
+            s+=ans[i];
         }
         return s;
     }
-};    
+};
