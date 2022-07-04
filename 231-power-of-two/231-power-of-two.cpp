a6012cca97150706811bd5f1c32cc72a0d4f38f0
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-07-04 08:56:15 +0530

    Time: 3 ms (58.61%), Space: 5.8 MB (88.28%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int n) {
-        if(n==0){
-            return false;
-        }
-        while(n%2==0){
-            n=n/2;
-        }
-        return n==1;
+     bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        return !(n&(n-1));
     }
 };
