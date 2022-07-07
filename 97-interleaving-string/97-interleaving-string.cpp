87748c30d100d7302a89d91be0cf7ddeedaa1931
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-07-07 08:43:56 +0530

    Time: 101 ms (10.63%), Space: 26 MB (21.22%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int dp[101][101][201];
-    int fun(int n,int m,int k,string s1,string s2,string s3){
+    bool fun(string s1,string s2,string s3,int n,int m,int k){
         if(n==0 && m==0 && k==0){
-            return 1;
+            return true;
         }
         if(dp[n][m][k]!=-1){
-            return dp[n][m][k]=dp[n][m][k];
+            return dp[n][m][k];
         }
-        int f=0;
-        if(m>0){
-            if(s2[m-1]==s3[k-1]){
-                if(fun(n,m-1,k-1,s1,s2,s3)==1){
-                    f=1;
-                }
-            }
-        }
+        bool f=false;
         if(n>0){
             if(s1[n-1]==s3[k-1]){
-                if(fun(n-1,m,k-1,s1,s2,s3)==1){
-                    f=1;
-                }
+                f=(f || fun(s1,s2,s3,n-1,m,k-1));
+            }
+        }
+        if(m>0){
+            if(s2[m-1]==s3[k-1]){
+                f=(f || fun(s1,s2,s3,n,m-1,k-1));
             }
         }
         return dp[n][m][k]=f;
@@ -30,6 +26,6 @@ public:
         if(s1.length()+s2.length()!=s3.length()){
             return false;
         }
-        return fun(s1.length(),s2.length(),s3.length(),s1,s2,s3);
+        return fun(s1,s2,s3,s1.length(),s2.length(),s3.length());
     }
 };
