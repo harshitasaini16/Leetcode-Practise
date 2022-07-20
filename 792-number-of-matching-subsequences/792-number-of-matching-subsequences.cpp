b502407b4d4a81c9a26e82ca80c791e060ed29f2
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-07-20 22:54:49 +0530

    Time: 480 ms (39.02%), Space: 49.9 MB (48.09%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,23 +1,46 @@
 class Solution {
 public:
-    	int numMatchingSubseq (string S, vector<string>& words) {
-		vector<vector<int>> alpha (26);
-		for (int i = 0; i < S.size (); ++i) alpha[S[i] - 'a'].push_back (i);
-		int res = 0;
-
-		for (const auto& word : words) {
-			int x = -1;
-			bool found = true;
-
-			for (char c : word) {
-				auto it = upper_bound (alpha[c - 'a'].begin (), alpha[c - 'a'].end (), x);
-				if (it == alpha[c - 'a'].end ()) found = false;
-				else x = *it;
-			}
-
-			if (found) res++;
-		}
-
-		return res;
-	}
+int fun(vector<int>&arr,int prev){
+int i=0,j=arr.size()-1;
+int ans=INT_MAX;
+while(i<=j){
+int m=i+(j-i)/2;
+if(arr[m]>prev){
+ans=arr[m];
+j=m-1;
+}else{
+i=m+1;
+}
+}
+return ans;
+}
+int numMatchingSubseq(string s, vector<string>& arr) {
+map<char,vector<int>>mp;
+for(int i=0;i<s.length();i++){
+mp[s[i]].push_back(i);
+}
+int ans=0;
+for(auto x:arr){
+int prev=-1;
+bool f=true;
+for(auto y:x){
+if(mp.find(y)==mp.end()){
+f=false;
+break;
+}else{
+int k=fun(mp[y],prev);
+// cout<<y<<" "<<k<<endl;
+if(k==INT_MAX){
+f=false;
+break;
+}
+prev=k;
+}
+}
+if(f==true){
+ans++;
+}
+}
+return ans;
+}
 };
