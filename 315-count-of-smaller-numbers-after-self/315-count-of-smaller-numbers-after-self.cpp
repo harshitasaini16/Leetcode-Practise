b9682a77a6a3a925a36122ef4b1ecf2be971ec2d
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-07-23 12:16:47 +0530

    Time: 894 ms (46.02%), Space: 170.7 MB (51.53%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,55 +1,40 @@
 class Solution {
+protected:
+    void merge_countSmaller(vector<int>& indices, int first, int last, 
+                            vector<int>& results, vector<int>& nums) {
+        int count = last - first;
+        if (count > 1) {
+            int step = count / 2;
+            int mid = first + step;
+            merge_countSmaller(indices, first, mid, results, nums);
+            merge_countSmaller(indices, mid, last, results, nums);
+            vector<int> tmp;
+            tmp.reserve(count);
+            int idx1 = first;
+            int idx2 = mid;
+            int semicount = 0;
+            while ((idx1 < mid) || (idx2 < last)) {
+                if ((idx2 == last) || ((idx1 < mid) &&
+                       (nums[indices[idx1]] <= nums[indices[idx2]]))) {
+					tmp.push_back(indices[idx1]);
+                    results[indices[idx1]] += semicount;
+                    ++idx1;
+                } else {
+					tmp.push_back(indices[idx2]);
+                    ++semicount;
+                    ++idx2;
+                }
+            }
+            move(tmp.begin(), tmp.end(), indices.begin()+first);
+        }
+    }
 public:
-    void add(int l,int m,int r,vector<pair<int,int>>&v,vector<int>&count){
-        vector<pair<int,int>>tmp(r-l+1);
-        int i=l;
-        int j=m+1;
-        int k=0;
-        while(i<=m && j<=r){
-            if(v[i].first<=v[j].first){
-                tmp[k]=v[j];
-                j++;
-                k++;
-            }
-            else{
-                count[v[i].second]+=(r-j+1);
-                tmp[k]=v[i];
-                i++;
-                k++;
-            }
-        }
-        while(i<=m){
-            tmp[k]=v[i];
-            i++;
-            k++;
-        }
-        while(j<=r){
-            tmp[k]=v[j];
-            j++;
-            k++;
-        }
-        for(int i=l;i<=r;i++){
-            v[i]=tmp[i-l];
-        }
-    }
-    void merge(int l,int r, vector<pair<int,int>>&v,vector<int>& count){
-        if(l<r){
-            int mid=l+(r-l)/2;
-            merge(l,mid,v,count);
-            merge(mid+1,r,v,count);
-            add(l,mid,r,v,count);
-        }
-        return;
-    }
     vector<int> countSmaller(vector<int>& nums) {
-        vector<int>count(nums.size(),0);
         int n = nums.size();
-
-        vector<pair<int, int> > v(n);
-        for (int i = 0; i < n; i++)   
-            v[i] = make_pair(nums[i], i);
-
-        merge(0,n-1,v,count);
-        return count;
+        vector<int> results(n, 0);
+        vector<int> indices(n, 0);
+        iota(indices.begin(), indices.end(), 0);
+        merge_countSmaller(indices, 0, n, results, nums);
+        return results;
     }
 };
