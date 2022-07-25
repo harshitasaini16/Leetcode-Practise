aca2ccf30e02a5c9de4314b62607184760c4b0f7
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-07-25 09:07:50 +0530

    Time: 205 ms (54.93%), Space: 22.9 MB (44.41%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,27 +2,34 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n=s.length();
-        string ans="";
-        for(int i=0;i<s.length();i++){
-            int x=i-1;
-            int y=i+1;
-            while(x>=0 && y<n && s[x]==s[y]){
-                x--;
-                y++;
+        int ans=1;
+        string st=s.substr(0,1);
+        for(int i=0;i<n;i++){
+            int l=i;
+            int r=i+1;
+            while(l>=0 && r<n && s[l]==s[r]){
+                l--;
+                r++;
             }
-            if(y-x-1>ans.length()){
-                ans=s.substr(x+1,y-x-1);
+            string sc=s.substr(l+1,r-l-1);
+            cout<<sc<<' ';
+            if(sc.length()>ans){
+                ans=sc.length();
+                st=sc;
             }
-            x=i;
-            y=i+1;
-            while(x>=0 && y<n && s[x]==s[y]){
-                x--;
-                y++;
+            l=i-1;
+            r=i+1;
+            while(l>=0 && r<n && s[l]==s[r]){
+                l--;
+                r++;
             }
-            if(y-x-1>ans.length()){
-                ans=s.substr(x+1,y-x-1);
+            sc=s.substr(l+1,r-l-1);
+            cout<<sc<<' ';
+            if(sc.length()>ans){
+                ans=sc.length();
+                st=sc;
             }
         }
-        return ans;
+        return st;
     }
 };
