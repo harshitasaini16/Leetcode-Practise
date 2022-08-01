ddf4ed0e76b3cd0fd39c02822c130a652d6e96a6
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-08-01 14:41:25 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (62.57%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n=digits.size();
         vector<int>ans;
         int c=1;
+        int n=digits.size();
         for(int i=n-1;i>=0;i--){
             int x=digits[i]+c;
             ans.push_back(x%10);
