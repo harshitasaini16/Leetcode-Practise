54d8bf3d8a52569bb4da292427b880674618e6ce
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-08-05 08:14:39 +0530

    Time: 228 ms (77.87%), Space: 81.4 MB (46.31%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -33,9 +33,9 @@ public:
         }
         int ans=0;
         for(int i=0;i<n;i++){
-            int left=(l[i]+1);
-            int right=(r[i]-1);
-            int x=right-left+1;
+            int left=l[i];
+            int right=r[i];
+            int x=right-left-1;
             ans=max(ans,heights[i]*x);
         }
         return ans;
