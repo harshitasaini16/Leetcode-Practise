6295b15a61a458355168b39662f1aa85643067ea
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-08-13 10:17:31 +0530

    Time: 506 ms (73.69%), Space: 45.1 MB (89.63%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int ans;
-    void f(int l,int m,int r,vector<int>& nums){
+    int f(int l,int m,int r,vector<int>& nums){
         int x=l;
         int y=m+1;
+        int s=0;
         while(x<=m && y<=r){
             if((long long)nums[x]>((long long)2*nums[y])){
-                ans+=(m-x+1);
+                s+=(m-x+1);
                 y++;
             }
             else{
@@ -14,18 +14,19 @@ public:
             }
         }
         sort(nums.begin()+l,nums.begin()+r+1);
+        return s;
     }
-    void fun(int l,int r,vector<int>& nums){
+    int fun(int l,int r,vector<int>& nums){
         if(l<r){
             int mid=l+(r-l)/2;
-            fun(l,mid,nums);
-            fun(mid+1,r,nums);
-            f(l,mid,r,nums);
+            int x1=fun(l,mid,nums);
+            int x2=fun(mid+1,r,nums);
+            int x3=f(l,mid,r,nums);
+            return x1+x2+x3;
         }
+        return 0;
     }
     int reversePairs(vector<int>& nums) {
-        ans=0;
-        fun(0,nums.size()-1,nums);
-        return ans;
+        return fun(0,nums.size()-1,nums);
     }
 };
