e3fd251f217bf1b1baf1d75898b9191d35da5ba8
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-08-20 10:24:07 +0530

    Time: 67 ms (32.14%), Space: 70.9 MB (97.59%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -10,35 +10,30 @@
  */
 class Solution {
 public:
-    ListNode *fun(ListNode* l1, ListNode* l2,int x){
+    ListNode* fun(ListNode* l1, ListNode* l2,int c){
         if(l1==NULL && l2==NULL){
-            if(x!=0){
-                ListNode *n=new ListNode(x);
+            if(c>0){
+                ListNode *n=new ListNode(c);
                 return n;
             }
-            else{
-                return NULL;
-            }
+            return NULL;
         }
-        if(l1!=NULL && l2==NULL){
-            int s=l1->val+x;
-            l1->val=s%10;
-            l1->next=fun(l1->next,l2,s/10);
-            return l1;
-        }
-        if(l1==NULL && l2!=NULL){
-            int s=l2->val+x;
-            l2->val=s%10;
-            l2->next=fun(l1,l2->next,s/10);
+        if(l1==NULL){
+            int x=l2->val+c;
+            l2->val=x%10;
+            l2->next=fun(l1,l2->next,x/10);
             return l2;
         }
-        if(l1!=NULL && l2!=NULL){
-            int s=l1->val+l2->val+x;
-            l1->val=s%10;
-            l1->next=fun(l1->next,l2->next,s/10);
+        if(l2==NULL){
+            int x=l1->val+c;
+            l1->val=x%10;
+            l1->next=fun(l1->next,l2,x/10);
             return l1;
         }
-        return NULL;
+        int x=l1->val+l2->val+c;
+        l1->val=x%10;
+        l1->next=fun(l1->next,l2->next,x/10);
+        return l1;
     }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         return fun(l1,l2,0);
