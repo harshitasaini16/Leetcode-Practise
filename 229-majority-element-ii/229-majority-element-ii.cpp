3e330be905310781ea35411bd5248dc6ce922140
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-08-23 15:44:50 +0530

    Time: 12 ms (91.63%), Space: 15.7 MB (82.32%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int count1=0,a=0,count2=0,b=0;
+        int c1=0,c2=0,x1=0,x2=0;
         int n=nums.size();
         for(int i=0;i<n;i++){
-            if(count1>0 && a==nums[i]){
-                count1++;
+            if(nums[i]==x1){
+                c1++;
             }
-            else if(count2>0 && b==nums[i]){
-                count2++;
+            else if(nums[i]==x2){
+                c2++;
             }
-            else if(count1==0){
-                count1++;
-                a=nums[i];
+            else if(c1==0){
+                c1++;
+                x1=nums[i];
             }
-            else if(count2==0){
-                count2++;
-                b=nums[i];
+            else if(c2==0){
+                c2++;
+                x2=nums[i];
             }
+            
             else{
-                count1--;
-                count2--;
+                c1--;
+                c2--;
             }
         }
-        int s1=0,s2=0;
+        int d1=0,d2=0;
         for(int i=0;i<n;i++){
-            if(nums[i]==a){
-                s1++;
+            if(nums[i]==x1){
+                d1++;
             }
-            if(nums[i]==b){
-                s2++;
+            else if(nums[i]==x2){
+                d2++;
             }
         }
+        int p=n/3;
         vector<int>v;
-        if(s1>(n/3)){
-            v.push_back(a);
+        if(d1>p){
+            v.push_back(x1);
         }
-        if(s2>(n/3) && a!=b){
-            v.push_back(b);
+        if(d2>p && x1!=x2 && d2>0){
+            v.push_back(x2);
         }
         return v;
     }
