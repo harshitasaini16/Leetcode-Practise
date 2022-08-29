33ddec1b75b2b3c269a60cda780f4802a052f803
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-08-29 22:21:40 +0530

    Time: 58 ms (47.98%), Space: 18.2 MB (17.24%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int dx[4]={-1,0,0,1};
-    int dy[4]={0,-1,1,0};
-    void fun(int x,int y,vector<vector<char>>& grid){
-        if(x<0 || y<0 || x>=grid.size() || y>=grid[0].size() || grid[x][y]=='0'){
-            return;
-        }
-        grid[x][y]='0';
-        for(int i=0;i<4;i++){
-            int xx=x+dx[i];
-            int yy=y+dy[i];
-            fun(xx,yy,grid);
-        }
-    }
     int numIslands(vector<vector<char>>& grid) {
+        int dx[4]={-1,0,0,1};
+        int dy[4]={0,-1,1,0};
         int ans=0;
         int n=grid.size();
         int m=grid[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1'){
+                    grid[i][j]='0';
                     ans++;
-                    fun(i,j,grid);
+                    queue<pair<int,int>>q;
+                    q.push({i,j});
+                    while(!q.empty()){
+                        int x=q.front().first;
+                        int y=q.front().second;
+                        q.pop();
+                        for(int g=0;g<4;g++){
+                            int xx=x+dx[g];
+                            int yy=y+dy[g];
+                            if(xx>=0 && yy>=0 && xx<n && yy<m && grid[xx][yy]=='1'){
+                                q.push({xx,yy});
+                                grid[xx][yy]='0';
+                            }
+                        }
+                    }
                 }
             }
         }
