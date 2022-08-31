a7bbd457fdab523b779338890f58f9d8da9842eb
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-08-31 15:06:05 +0530

    Time: 69 ms (58.55%), Space: 17.7 MB (72.42%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    void fun(int x,int y,vector<vector<bool>>&vis,vector<vector<int>>h,int n,int m){
-        vis[x][y]=true;
-        if(x+1<n && y<m && x+1>=0 && y>=0 && vis[x+1][y]==false && h[x][y]<=h[x+1][y]){
-            fun(x+1,y,vis,h,n,m);
-        }
-        if(x-1<n && y<m && x-1>=0 && y>=0 && vis[x-1][y]==false && h[x][y]<=h[x-1][y]){
-            fun(x-1,y,vis,h,n,m);
-        }
-        if(x<n && y+1<m && x>=0 && y>=0 && vis[x][y+1]==false && h[x][y]<=h[x][y+1]){
-            fun(x,y+1,vis,h,n,m);
-        }
-        if(x<n && y-1<m && x>=0 && y-1>=0 && vis[x][y-1]==false && h[x][y]<=h[x][y-1]){
-            fun(x,y-1,vis,h,n,m);
+    int dx[4]={-1,0,0,1};
+    int dy[4]={0,1,-1,0};
+    
+    void fun(int x,int y,vector<vector<bool>>&v,int n,int m,vector<vector<int>>& heights){
+        v[x][y]=true;
+        for(int i=0;i<4;i++){
+            int xx=x+dx[i];
+            int yy=y+dy[i];
+            if(xx>=0 && yy>=0 && xx<n && yy<m && v[xx][yy]==false && heights[x][y]<=heights[xx][yy]){
+                fun(xx,yy,v,n,m,heights);
+            }
         }
     }
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& h) {
-        int n=h.size();
-        int m=h[0].size();
-        vector<vector<bool>>visp(n,vector<bool>(m,false));
-        vector<vector<bool>>visa(n,vector<bool>(m,false));
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int n=heights.size();
+        int m=heights[0].size();
+        vector<vector<bool>>p(n,vector<bool>(m,false));
+        vector<vector<bool>>a(n,vector<bool>(m,false));
         for(int i=0;i<n;i++){
-            fun(i,0,visp,h,n,m);
-            fun(i,m-1,visa,h,n,m);
+            fun(i,0,p,n,m,heights);
+            fun(i,m-1,a,n,m,heights);
         }
         for(int i=0;i<m;i++){
-            fun(0,i,visp,h,n,m);
-            fun(n-1,i,visa,h,n,m);
+            fun(0,i,p,n,m,heights);
+            fun(n-1,i,a,n,m,heights);
         }
-        vector<vector<int>>v;
+        vector<vector<int>>ans;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(visp[i][j]==true && visa[i][j]==true){
-                    v.push_back({i,j});
+                if(a[i][j]==true && p[i][j]==true){
+                    ans.push_back({i,j});
                 }
             }
         }
-        return v;
+        return ans;
     }
 };
