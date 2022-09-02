4cf3ae15310fb4b1d0c5a8a5facd1b8f979b2bd1
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-09-02 21:21:20 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (5.29%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,59 +1,60 @@
 class Solution {
 public:
-    void fun(int i,string digits,string s,vector<string>&v){
-        if(i==digits.size()){
-            v.push_back(s);
+    vector<string>ans;
+    void fun(int x,string digits,string s){
+        if(x>=digits.length()){
+            if(s!=""){
+                ans.push_back(s);
+            }
             return;
         }
-        if(digits[i]=='2'){
-             fun(i+1,digits,s+'a',v);
-             fun(i+1,digits,s+'b',v);
-             fun(i+1,digits,s+'c',v);
+        if(digits[x]=='2'){
+            fun(x+1,digits,s+'a');
+            fun(x+1,digits,s+'b');
+            fun(x+1,digits,s+'c');
         }
-        if(digits[i]=='3'){
-            fun(i+1,digits,s+'d',v);
-             fun(i+1,digits,s+'e',v);
-             fun(i+1,digits,s+'f',v);
+        if(digits[x]=='3'){
+            fun(x+1,digits,s+'d');
+            fun(x+1,digits,s+'e');
+            fun(x+1,digits,s+'f');
         }
-        if(digits[i]=='4'){
-            fun(i+1,digits,s+'g',v);
-             fun(i+1,digits,s+'h',v);
-             fun(i+1,digits,s+'i',v);
+        if(digits[x]=='4'){
+            fun(x+1,digits,s+'g');
+            fun(x+1,digits,s+'h');
+            fun(x+1,digits,s+'i');
         }
-        if(digits[i]=='5'){
-            fun(i+1,digits,s+'j',v);
-             fun(i+1,digits,s+'k',v);
-             fun(i+1,digits,s+'l',v);
+        if(digits[x]=='5'){
+            fun(x+1,digits,s+'j');
+            fun(x+1,digits,s+'k');
+            fun(x+1,digits,s+'l');
         }
-        if(digits[i]=='6'){
-            fun(i+1,digits,s+'m',v);
-             fun(i+1,digits,s+'n',v);
-             fun(i+1,digits,s+'o',v);
+        if(digits[x]=='6'){
+            fun(x+1,digits,s+'m');
+            fun(x+1,digits,s+'n');
+            fun(x+1,digits,s+'o');
         }
-        if(digits[i]=='7'){
-            fun(i+1,digits,s+'p',v);
-             fun(i+1,digits,s+'q',v);
-             fun(i+1,digits,s+'r',v);
-            fun(i+1,digits,s+'s',v);
+        if(digits[x]=='7'){
+            fun(x+1,digits,s+'p');
+            fun(x+1,digits,s+'q');
+            fun(x+1,digits,s+'r');
+            fun(x+1,digits,s+'s');
         }
-        if(digits[i]=='8'){
-            fun(i+1,digits,s+'t',v);
-             fun(i+1,digits,s+'u',v);
-             fun(i+1,digits,s+'v',v);
+        if(digits[x]=='8'){
+            fun(x+1,digits,s+'t');
+            fun(x+1,digits,s+'u');
+            fun(x+1,digits,s+'v');
         }
-        if(digits[i]=='9'){
-            fun(i+1,digits,s+'w',v);
-             fun(i+1,digits,s+'x',v);
-             fun(i+1,digits,s+'y',v);
-             fun(i+1,digits,s+'z',v);
+        if(digits[x]=='9'){
+            fun(x+1,digits,s+'w');
+            fun(x+1,digits,s+'x');
+            fun(x+1,digits,s+'y');
+            fun(x+1,digits,s+'z');
         }
     }
     vector<string> letterCombinations(string digits) {
-        vector<string>v;
+        ans.clear();
         string s="";
-        fun(0,digits,s,v);
-        if(digits.size()==0)
-            return {};
-        return v;
+        fun(0,digits,s);
+        return ans;
     }
 };
