5072b34eea3eb5c247243de851efdeb7a567fa9b
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-09-02 17:11:06 +0530

    Time: 87 ms (15.45%), Space: 89.2 MB (97.10%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    double fun(vector<int>& nums1,int n, vector<int>& nums2,int m){
-        if(m<n){
-            return fun(nums2,m,nums1,n);
+    double fun(vector<int>& nums1, vector<int>& nums2){
+        if(nums2.size()<nums1.size()){
+            return fun(nums2,nums1);
         }
+        int n=nums1.size();
+        int m=nums2.size();
+        int k=(n+m+1)/2;
         int l=0;
         int r=n;
-        int k=(n+m+1)/2;
         while(l<=r){
-            int m1=l+(r-l)/2;
-            int m2=k-m1;
-            int l1=(m1==0)?INT_MIN:nums1[m1-1];
-            int l2=(m2==0)?INT_MIN:nums2[m2-1];
-            int r1=(m1==n)?INT_MAX:nums1[m1];
-            int r2=(m2==m)?INT_MAX:nums2[m2];
-            if(l1<=r2 && l2<=r1){
+            int cut1=l+(r-l)/2;
+            int cut2=k-cut1;
+            int mn1=(cut1==0)?INT_MIN:nums1[cut1-1];
+            int mx1=(cut1==n)?INT_MAX:nums1[cut1];
+            int mn2=(cut2==0)?INT_MIN:nums2[cut2-1];
+            int mx2=(cut2==m)?INT_MAX:nums2[cut2];
+            if(mn1<=mx2 && mn2<=mx1){
                 if((n+m)%2==0){
-                    return (double)(max(l1,l2)+min(r1,r2))/(double)2;
+                    return (double)(max(mn1,mn2)+min(mx1,mx2))/(double)2;
                 }
                 else{
-                    return (double)max(l1,l2);
+                    return max(mn1,mn2);
                 }
             }
-            else if(l1>r2){
-                r=m1-1;
-            }   
+            else if(mn1>mx2){
+                r=cut1-1;
+            }
             else{
-                l=m1+1;
+                l=cut1+1;
             }
         }
         return 0.0;
     }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        return fun(nums1,nums1.size(),nums2,nums2.size());
+        return fun(nums1,nums2);
     }
 };
