8acfba7321c862e854e9c942b040e3dfe9058e30
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-09-03 09:12:09 +0530

    Time: 4 ms (31.94%), Space: 6.4 MB (8.72%) - LeetHub

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,10 +3,42 @@ public:
     int strStr(string s1, string s2) {
         int n=s1.length();
         int m=s2.length();
-        for(int i=0;i<=n-m;i++){
-            string g=s1.substr(i,m);
-            if(g==s2){
-                return i;
+        vector<int>lps(m,0);
+        int len=0;
+        int i=1;
+        while(i<m){
+            if(s2[i]==s2[len]){
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else{
+                if(len==0){
+                    lps[i]=0;
+                    i++;
+                }
+                else{
+                    len=lps[len-1];
+                }
+            }
+        }
+        i=0;
+        int j=0;
+        while(i<n && j<m){
+            if(s1[i]==s2[j]){
+                i++;
+                j++;
+            }
+            if(j==m){
+                return i-m;
+            }
+            else if(i<n && s1[i]!=s2[j]){
+                if(j==0){
+                    i++;
+                }
+                else{
+                    j=lps[j-1];
+                }
             }
         }
         return -1;
