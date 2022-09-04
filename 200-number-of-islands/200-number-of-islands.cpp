335c9b938e888f60965ace334f97fda36ed034e1
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-09-04 12:02:52 +0530

    Time: 43 ms (77.76%), Space: 18.1 MB (15.94%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -3,24 +3,24 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int dx[4]={-1,0,0,1};
         int dy[4]={0,-1,1,0};
-        int ans=0;
         int n=grid.size();
+        int ans=0;
         int m=grid[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1'){
-                    grid[i][j]='0';
                     ans++;
                     queue<pair<int,int>>q;
                     q.push({i,j});
+                    grid[i][j]='0';
                     while(!q.empty()){
                         int x=q.front().first;
                         int y=q.front().second;
                         q.pop();
-                        for(int g=0;g<4;g++){
-                            int xx=x+dx[g];
-                            int yy=y+dy[g];
-                            if(xx>=0 && yy>=0 && xx<n && yy<m && grid[xx][yy]=='1'){
+                        for(int k=0;k<4;k++){
+                            int xx=dx[k]+x;
+                            int yy=dy[k]+y;
+                            if(xx<n && yy<m && xx>=0 && yy>=0 && grid[xx][yy]=='1'){
                                 q.push({xx,yy});
                                 grid[xx][yy]='0';
                             }
