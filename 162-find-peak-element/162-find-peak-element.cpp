481cc1dc554ca063c4ea30ec525c11d6d91caace
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-09-04 13:03:23 +0530

    Time: 16 ms (5.26%), Space: 8.9 MB (51.42%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -6,7 +6,7 @@ public:
         if(n==1){
             return 0;
         }
-        int r=n-1;
+        int r=nums.size()-1;
         while(l<=r){
             int mid=l+(r-l)/2;
             if(mid==0){
@@ -14,25 +14,25 @@ public:
                     return mid;
                 }
                 else{
-                    l++;
+                    l=mid+1;
                 }
             }
             else if(mid==n-1){
-                if(nums[mid]>nums[mid-1]){
+                if(nums[mid-1]<nums[mid]){
                     return mid;
                 }
                 else{
-                    r--;
+                    r=mid-1;
                 }
             }
-            else if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
+            else if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
                 return mid;
             }
-            else if(nums[mid]<nums[mid+1]){
-                l++;
+            else if(nums[mid-1]>nums[mid]){
+                r=mid-1;
             }
             else{
-                r--;
+                l=mid+1;
             }
         }
         return -1;
