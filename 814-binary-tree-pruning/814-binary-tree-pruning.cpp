0d2f3d37d1451bf7c13de60d60fa0fe8a6822150
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-09-06 08:58:11 +0530

    Time: 5 ms (36.34%), Space: 8.8 MB (21.01%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.cpp
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -1,23 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
+    bool fun(TreeNode*root){
+        if(root==NULL){
+            return false;
+        }
+        if(root->left==NULL and root->right==NULL){
+            if(root->val==1){
+                return true;
+            }
+            return false;
+        }
+        bool l=fun(root->left);
+        bool r=fun(root->right);
+        if(l==0){
+            root->left=NULL;
+        }
+        if(r==0){
+            root->right=NULL;
+        }
+        if(root->val==1 || l==true || r==true){
+            return true;
+        }
+        return false;
+    }
     TreeNode* pruneTree(TreeNode* root) {
         if(root==NULL){
             return NULL;
         }
-        root->left=pruneTree(root->left);
-        root->right=pruneTree(root->right);
-        if(root->left!=NULL || root->right!=NULL || root->val==1){
+        if(fun(root)){
             return root;
         }
         return NULL;
