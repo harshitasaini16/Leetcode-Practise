3afd60e825557a1f7f35ecf70fdf6641e9fe41e0
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-09-10 22:34:05 +0530

    Time: 21 ms (48.09%), Space: 12.2 MB (52.05%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    int dp[1001][1001][3];
-    int fun(int x,vector<int>& prices,int cap,int v){
-        if(x>=prices.size()){
+    int dp[1001][101][3];
+    int fun(int x, vector<int>& prices,int k,int f){
+        if(x>=prices.size() || k==0){
             return 0;
         }
-        if(cap==0){
-            return 0;
+        if(dp[x][k][f]!=-1){
+            return dp[x][k][f];
         }
-        if(dp[x][cap][v]!=-1){
-            return dp[x][cap][v];
-        }
-        if(v==0){
-            return dp[x][cap][v]=max(-prices[x]+fun(x+1,prices,cap,1),fun(x+1,prices,cap,0));
+        if(f==0){
+            return dp[x][k][f]=max(-prices[x]+fun(x+1,prices,k,1),fun(x+1,prices,k,0));
         }
         else{
-            return dp[x][cap][v]=max(prices[x]+fun(x+1,prices,cap-1,0),fun(x+1,prices,cap,1));
+            return dp[x][k][f]=max(prices[x]+fun(x+1,prices,k-1,0),fun(x+1,prices,k,1));
         }
     }
     int maxProfit(int k, vector<int>& prices) {
-        int n=prices.size();
         memset(dp,-1,sizeof(dp));
         return fun(0,prices,k,0);
     }
