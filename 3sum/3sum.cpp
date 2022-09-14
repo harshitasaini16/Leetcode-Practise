9ac743ad555faffe676210f810d73166722115ad
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-09-14 12:03:39 +0530

    Time: 117 ms, Memory: 19.7 MB - LeetHub

---
+++ 3sum/3sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        int n=nums.size();
+        for(int i=0;i<n-2;i++){
+            if(i!=0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                if(nums[i]+nums[j]+nums[k]==0){
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    int x=nums[j];
+                    while(j<k & x==nums[j]){
+                        j++;
+                    }
+                    int y=nums[k];
+                    while(j<k && nums[k]==y){
+                        k--;
+                    }
+                }
+                else if(nums[i]+nums[j]+nums[k]>0){
+                    k--;
+                }
+                else{
+                    j++;
+                }
+            }
+        }
+        return ans;
+    }
+};
