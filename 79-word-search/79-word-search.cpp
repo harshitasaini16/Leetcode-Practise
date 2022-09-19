bf995a1f0111f59a461b0616018782bbffc1db7f
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-09-19 14:36:57 +0530

    Time: 677 ms (48.51%), Space: 8 MB (47.41%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int dx[4]={-1,0,0,1};
-    int dy[4]={0,1,-1,0};
-    bool fun(int x,int y,vector<vector<char>>&board,string s,int k){
-        if(k>=s.length()){
-            return true;
-        }
-        if(x<0 || y<0 || x>=board.size() || y>=board[0].size() || board[x][y]!=s[k]){
+    int dy[4]={0,-1,1,0};
+    bool fun(int x,int y,vector<vector<char>>& board, string word,int k){
+        if(x<0 || y<0 || x>=board.size() || y>=board[0].size() || board[x][y]!=word[k]){
             return false;
         }
+        if(k==word.length()-1){
+            return true;
+        }
         char c=board[x][y];
-        if(board[x][y]==s[k]){
-            board[x][y]='.';
-            for(int i=0;i<4;i++){
-                if(fun(x+dx[i],y+dy[i],board,s,k+1)){
-                    return true;
-                }
+        board[x][y]='.';
+        for(int i=0;i<4;i++){
+            int xx=x+dx[i];
+            int yy=y+dy[i];
+            if(fun(xx,yy,board,word,k+1)){
+                return true;
             }
         }
         board[x][y]=c;
