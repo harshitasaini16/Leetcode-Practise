c4bbf32c3a575c53b764df0c1ffdab72c3ff6390
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2022-09-30 21:55:01 +0530

    Time: 136 ms (9.53%), Space: 18.8 MB (13.91%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.cpp
+++ 218-the-skyline-problem/218-the-skyline-problem.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-    bool cmp(vector<int>v1,vector<int>v2){
-        if(v1[0]==v2[0]){
+    bool static cmp(vector<int>&v1,vector<int>&v2){
+        if(v1[0]<v2[0]){
             return true;
         }
-        if(v1[0]==v2[0]){
-            if(v1[1]<v2[1]){
-                return true;
-            }
+        else if(v1[0]==v2[0]){
+            return v1[1]<v2[1];
         }
         return false;
     }
-    vector<vector<int>> getSkyline(vector<vector<int>>& b) {
-        vector<pair<int,int>>v;
-        for(int i=0;i<b.size();i++){
-            v.push_back({b[i][0],-b[i][2]});
-            v.push_back({b[i][1],b[i][2]});
+    vector<vector<int>> getSkyline(vector<vector<int>>& arr) {
+        vector<vector<int>>v;
+        for(auto x:arr){
+            v.push_back({x[0],-x[2]});
+            v.push_back({x[1],x[2]});
         }
-        sort(v.begin(),v.end());
-        multiset<int>s;
-        s.insert(0);
+        sort(v.begin(),v.end(),cmp);
+        multiset<int>s{0};
         vector<vector<int>>ans;
-        int mx=0;
-        for(int i=0;i<v.size();i++){
-            if(v[i].second<0){
-                s.insert(abs(v[i].second));
+        int top=0;
+        for(auto x:v){
+            if(x[1]<0){
+                s.insert(-x[1]);
+            }else{
+                s.erase(s.find(x[1]));
             }
-            else{
-                s.erase(s.find(v[i].second));
-            }
-            if(mx!=*s.rbegin()){
-                mx=*s.rbegin();
-                ans.push_back({v[i].first,mx});
+            if(*s.rbegin()!=top){
+                top=*s.rbegin();
+                ans.push_back({x[0],*s.rbegin()});
             }
         }
         return ans;
     }
+    
 };
