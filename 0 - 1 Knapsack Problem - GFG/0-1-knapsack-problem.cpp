57a2bd825f036b66b2646bee52089f91bcbd809c
Harshita saini <99383725+harshitasaini16@users.noreply.github.com>
2023-06-10 13:23:20 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -4,26 +4,28 @@ using namespace std;
 
 
 // } Driver Code Ends
-class Solution{
+class Solution
+{
     public:
     //Function to return max value that can be put in knapsack of capacity W.
     int dp[1001][1001];
-    int fun(int n,int *wt,int *val,int w){
+    int fun(int n,int w, int wt[], int val[]){
         if(n==0 || w==0){
             return 0;
         }
         if(dp[n][w]!=-1){
             return dp[n][w];
         }
-        if(wt[n-1]>w){
-            return dp[n][w]=fun(n-1,wt,val,w);
+        if(wt[n-1]<=w){
+            return dp[n][w]=max(fun(n-1,w,wt,val),val[n-1]+fun(n-1,w-wt[n-1],wt,val));
         }
-        return dp[n][w]=max(fun(n-1,wt,val,w),val[n-1]+fun(n-1,wt,val,w-wt[n-1]));
+        return dp[n][w]=fun(n-1,w,wt,val);
     }
-    int knapSack(int w, int wt[], int val[], int n){ 
+    int knapSack(int w, int wt[], int val[], int n) 
+    { 
        // Your code here
        memset(dp,-1,sizeof(dp));
-       return fun(n,wt,val,w);
+       return fun(n,w,wt,val);
     }
 };
 
